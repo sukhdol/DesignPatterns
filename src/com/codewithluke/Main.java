794bd7444fd6d04f0408e3d01e25b69001d45f04
Luke Dol <sukh.dol77@gmail.com>
2020-06-30 02:38:36 -0600

    Implemented Memento Pattern

--- src/com/codewithluke/Main.java
+++ src/com/codewithluke/Main.java
@@ -1,8 +1,26 @@
 package com.codewithluke;
 
+import com.codewithluke.memento.Editor;
+import com.codewithluke.memento.History;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var editor = new Editor();
+        var history = new History();
+        editor.setContent("a");
+
+        history.push(editor.createState());
+
+        editor.setContent("b");
+
+        history.push(editor.createState());
+
+        editor.setContent("c");
+
+        editor.restore(history.pop());
+        editor.restore(history.pop());
+
+        System.out.println("Last Content: " + editor.getContent());
     }
 }
---
+++ src/com/codewithluke/memento/EditorState.java
@@ -0,0 +1,13 @@
+package com.codewithluke.memento;
+
+public class EditorState {
+    private String content;
+
+    public EditorState(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
